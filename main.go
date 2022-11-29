0ac55ee4f21b3747944ef51c310459faed0abd46
foucaud_e <efoucaud@ifotec.com>
2022-11-29 10:53:09 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+// BLAST_API is the URL of the BLAST API
+const EXPLORER_API = "https://api.elrond.com"
+
+type Governance struct {
+	erd      string
+	proposal string
+	vote     string
+	power    string
+}
+
+func main() {
+	var (
+		smartContract = "erd1qqqqqqqqqqqqqpgqdt9aady5jp7av97m7rqxh6e5ywyqsplz2jps5mw02n"
+		page          int
+		governance    []Governance
+	)
+
+	for {
+		txs, err := GetTransactionsAccounts(smartContract, strconv.Itoa(page))
+		if err != nil {
+			log.Err(err).Msg("GetStackingRewards() err")
+			panic(err)
+		}
+		for _, tx := range txs {
+			base64 := decodeBase64String(tx.Data)
+			if len(strings.Split(base64, "@")) > 3 {
+				governance = append(governance, Governance{
+					erd:      tx.Sender,
+					vote:     getVote(base64),
+					proposal: getProposal(base64),
+					power:    getPower(base64),
+				})
+			} else {
+				fmt.Println(base64)
+			}
+		}
+		page = page + len(txs)
+		if len(txs) != 50 {
+			break
+		}
+	}
+
+	// Write CSV
+	writeCSV(governance)
+
+	fmt.Println("Done")
+}
+
+func decodeBase64String(base64String string) string {
+	decodedString, _ := base64.StdEncoding.DecodeString(base64String)
+	return string(decodedString)
+}
+
+func getProposal(decoded string) string {
+	split := strings.Split(decoded, "@")
+	return split[1]
+}
+
+func getVote(decoded string) string {
+	split := strings.Split(decoded, "@")
+	switch split[2] {
+	case "":
+		return "yes"
+	case "01":
+		return "no"
+	case "03":
+		return "blank"
+	default:
+		return ""
+	}
+}
+
+func getPower(decoded string) string {
+	split := strings.Split(decoded, "@")
+	power, _ := strconv.ParseInt(split[3], 16, 64)
+	return strconv.FormatInt(power, 10)
+}
+
+func GetTransactionsAccounts(erd, from string) ([]Transactions, error) {
+	//Encode the data
+	var (
+		url = EXPLORER_API + "/accounts/" + erd + "/transactions?from=" + from + "&size=50&withScResults=true"
+	)
+
+	// Create a new request using http
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Err(err).Msg("Error when client create GET request to " + url)
+		return []Transactions{}, err
+	}
+
+	// Send req using http Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Err(err).Msg("Error when client do request to " + url)
+		return []Transactions{}, err
+	}
+	defer resp.Body.Close()
+
+	// Check error code
+	if resp.StatusCode != http.StatusOK {
+		var errorElrond ErrorElrond
+		body, _ := io.ReadAll(resp.Body)
+		_ = json.Unmarshal(body, &errorElrond)
+		return []Transactions{}, errors.New(errorElrond.Message)
+	}
+
+	// Convert JSON into struct
+	var transactions []Transactions
+	err = json.NewDecoder(resp.Body).Decode(&transactions)
+	if err != nil {
+		log.Err(err).Msg("Error when Decode JSON")
+		return []Transactions{}, err
+	}
+
+	return transactions, nil
+}
+
+func writeCSV(governance []Governance) {
+	// export to csv file
+	file, err := os.Create("governance.csv")
+	if err != nil {
+		log.Err(err).Msg("Create file err")
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	writer.Comma = ';'
+
+	for i, value := range governance {
+		if i == 0 {
+			err := writer.Write([]string{"erd", "proposal", "vote", "power"})
+			if err != nil {
+				log.Err(err).Msg("Write header err")
+				panic(err)
+			}
+		}
+		err := writer.Write([]string{value.erd, value.proposal, value.vote, value.power})
+		if err != nil {
+			log.Err(err).Msg("Write file err")
+			panic(err)
+		}
+	}
+}
---
+++ model.go
@@ -0,0 +1,79 @@
+package main
+
+type ErrorElrond struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+}
+
+type Transactions struct {
+	TxHash        string      `json:"txHash"`
+	GasLimit      int         `json:"gasLimit"`
+	GasPrice      int         `json:"gasPrice"`
+	GasUsed       int         `json:"gasUsed"`
+	MiniBlockHash string      `json:"miniblockHash"`
+	Nonce         int         `json:"nonce"`
+	Receiver      string      `json:"receiver"`
+	ReceiverShard int         `json:"receiverShard"`
+	Round         int         `json:"round"`
+	Sender        string      `json:"sender"`
+	SenderShard   int         `json:"senderShard"`
+	Signature     string      `json:"signature"`
+	Status        string      `json:"status"`
+	Value         string      `json:"value"`
+	Fee           string      `json:"fee"`
+	Timestamp     int         `json:"timestamp"`
+	Data          string      `json:"data"`
+	Function      string      `json:"function"`
+	Action        Action      `json:"action"`
+	Results       []Result    `json:"results"`
+	Operations    []Operation `json:"operations"`
+}
+type Action struct {
+	Category    string   `json:"category"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Arguments   Argument `json:"arguments"`
+}
+
+type Argument struct {
+	Transfers      []Transfer `json:"transfers"`
+	ProviderName   string     `json:"providerName"`
+	ProviderAvatar string     `json:"providerAvatar"`
+	Receiver       string     `json:"receiver"`
+}
+
+type Transfer struct {
+	Type       string `json:"type"`
+	Name       string `json:"name"`
+	Ticker     string `json:"ticker"`
+	Collection string `json:"collection"`
+	Identifier string `json:"identifier"`
+	Value      string `json:"value"`
+	Token      string `json:"token"`
+	Decimals   int    `json:"decimals"`
+}
+
+type Result struct {
+	Hash          string `json:"hash"`
+	Timestamp     int    `json:"timestamp"`
+	Nonce         int    `json:"nonce"`
+	GasLimit      int    `json:"gasLimit"`
+	GasPrice      int    `json:"gasPrice"`
+	Value         string `json:"value"`
+	Sender        string `json:"sender"`
+	Receiver      string `json:"receiver"`
+	Data          string `json:"data"`
+	PreTxHash     string `json:"preTxHash"`
+	OriginTxHash  string `json:"originTxHash"`
+	CallType      string `json:"callType"`
+	MiniBlockHash string `json:"miniblockHash"`
+}
+
+type Operation struct {
+	ID       string `json:"id"`
+	Action   string `json:"action"`
+	Type     string `json:"type"`
+	Sender   string `json:"sender"`
+	Receiver string `json:"receiver"`
+	Value    string `json:"value"`
+}
